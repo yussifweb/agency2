d87dbfa2409bb32b06266614568e5accebc66dcb
Yussif <djiceman.gh@gmail.com>
2020-11-09 17:50:05 +0000

    agency-applicant-details

--- applicant_details.php
+++ applicant_details.php
@@ -49,8 +49,7 @@ if( !$_SESSION['email'] ){
                 </div>
             </div>
             <div class="col-12 col-sm-9">
-                <div class="card">
-                    
+                <div class="card">                    
                     <?php 
                         
                         $id = $_GET['id'];
@@ -62,103 +61,178 @@ if( !$_SESSION['email'] ){
                             <div class="card-header text-center"><?php echo $applicant['f_name'].' '. $applicant['l_name']; ?></div>
                         <img class="details" src="applicants/<?php echo $applicant['image']; ?>" alt="<?php echo $applicant['f_name']; ?>">
 
-                    <div class="card-body">
+                    <div class="card-body ml-3">
                         <div class="row">
                             <div class="col-12 col-sm-6">
-                            <p class="text-center"><strong>First Name: </strong> <?php echo $applicant['f_name']; ?>
+                            <p class=""><strong>First Name: </strong> <?php echo $applicant['f_name']; ?>
                             <span class="mr-3"></span><strong>Last Name: </strong><?php echo $applicant['l_name']; ?></p>
                             </div>
                             <div class="col-12 col-sm-6">
-                            <p class="text-center"><strong>Date of Birth: </strong> <?php echo $applicant['dob']; ?>
+                            <p class=""><strong>Date of Birth: </strong> <?php echo $applicant['dob']; ?>
                             <span class="mr-3"></span><strong>Age: </strong><?php echo $applicant['age']; ?></p>
                             </div>    
                         </div>
                         <div class="row">
                             <div class="col-12 col-sm-6">
-                            <p class="text-center"><strong>Email: </strong> <?php echo $applicant['email']; ?>
+                            <p class=""><strong>Email: </strong> <?php echo $applicant['email']; ?>
                             <span class="mr-3"></span><strong>Contact: </strong><?php echo $applicant['phone']; ?></p>
                             </div>
                             <div class="col-12 col-sm-6">
-                            <p class="text-center"><strong>Country: </strong> <?php echo $applicant['country']; ?>
+                            <p class=""><strong>Country: </strong> <?php echo $applicant['country']; ?>
                             <span class="mr-3"></span><strong>Language: </strong><?php echo $applicant['dialect']; ?></p>
                             </div>    
                         </div>
                         <div class="row">
                             <div class="col-12 col-sm-6">
-                            <p class="text-center"><strong>Email: </strong> <?php echo $applicant['id_typeRadios']; ?>
+                            <p class=""><strong>Email: </strong> <?php echo $applicant['id_typeRadios']; ?>
                             <span class="mr-3"></span><strong>ID Number: </strong><?php echo $applicant['id_image']; ?></p>
                             </div>
                             <div class="col-12 col-sm-6">
-                            <p class="text-center"><strong>Gender: </strong> <?php echo $applicant['genderRadios']; ?>
+                            <p class=""><strong>Gender: </strong> <?php echo $applicant['genderRadios']; ?>
                             <span class="mr-3"></span><strong>Marital Status: </strong><?php echo $applicant['mar_statRadios']; ?></p>
                             </div>    
                         </div>
 
                         <div class="row">
                             <div class="col-12 col-sm-6">
-                            <p class="text-center"><strong>Spouse: </strong> <?php echo $applicant['spouse']; ?>
+                            <p class=""><strong>Spouse: </strong> <?php echo $applicant['spouse']; ?>
                             <span class="mr-3"></span><strong>Religion: </strong><?php echo $applicant['religion']; ?></p>
                             </div>
                             <div class="col-12 col-sm-6">
-                            <p class="text-center"><strong>Residence: </strong> <?php echo $applicant['residence']; ?>
+                            <p class=""><strong>Residence: </strong> <?php echo $applicant['residence']; ?>
                             <span class="mr-3"></span><strong>Address: </strong><?php echo $applicant['box_addr']; ?></p>
                             </div>    
                         </div>
 
                         <div class="row">
                             <div class="col-12 col-sm-6">
-                            <p class="text-center"><strong>LandMark: </strong> <?php echo $applicant['landmark']; ?>
+                            <p class=""><strong>LandMark: </strong> <?php echo $applicant['landmark']; ?>
                             <span class="mr-3"></span><strong>Street Name: </strong><?php echo $applicant['street_nm']; ?></p>
                             </div>
                             <div class="col-12 col-sm-6">
-                            <p class="text-center"><strong>Suburb: </strong> <?php echo $applicant['suburb']; ?>
-                            <span class="mr-2"></span><strong>House No: </strong><?php echo $applicant['hse_no']; ?>
-                            <span class="mr-2"></span><strong>City/Town: </strong><?php echo $applicant['city_town']; ?></p>
+                            <p class=""><strong>Suburb: </strong> <?php echo $applicant['suburb']; ?>
+                            <span class="mr-2"></span><strong>House No: </strong><?php echo $applicant['hse_no']; ?></p>
+                            </div>    
+                        </div>
+
+                        <div class="row">
+                        <div class="col-12 col-sm-12">
+                        <p><strong>City/Town: </strong><?php echo $applicant['city_town']; ?></p>
+                        
+                        </div>
+                        </div>
+
+                        <div class="row">
+                            <div class="col-12 col-sm-6">
+                            <p class=""><strong>Area of Interest 1: </strong> <?php echo $applicant['area_of_int_1']; ?></p>
+                            </div>
+                            <div class="col-12 col-sm-6">
+                            <p class=""><strong>Area of Interest 2: </strong> <?php echo $applicant['area_of_int_2']; ?></p>
                             </div>    
                         </div>
 
                         <div class="row">
                             <div class="col-12 col-sm-6">
-                            <p class="text-center"><strong>Area of Interest 1: </strong> <?php echo $applicant['area_of_int_1']; ?></p>
+                            <p class=""><strong>Name of JHS: </strong> <?php echo $applicant['jhs_nm']; ?></p>
                             </div>
                             <div class="col-12 col-sm-6">
-                            <p class="text-center"><strong>Area of Interest 2: </strong> <?php echo $applicant['area_of_int_2']; ?></p>
+                            <p class=""><strong>Awards Recieved: </strong> <?php echo $applicant['jhs_awd']; ?></p>
+                            </div>    
+                        </div>
+                        <div class="row">
+                            <div class="col-12 col-sm-6">
+                            <p class=""><strong>Start Year: </strong> <?php echo $applicant['jhs_start']; ?></p>
+                            </div>
+                            <div class="col-12 col-sm-6">
+                            <p class=""><strong>Year of Completion: </strong> <?php echo $applicant['jhs_end']; ?></p>
                             </div>    
                         </div>
 
                         <div class="row">
                             <div class="col-12 col-sm-6">
-                            <p class="text-center"><strong>Name of JHS: </strong> <?php echo $applicant['jhs_nm']; ?></p>
+                            <p class=""><strong>Name of SHS: </strong> <?php echo $applicant['shs_nm']; ?></p>
                             </div>
                             <div class="col-12 col-sm-6">
-                            <p class="text-center"><strong>Awards Recieved: </strong> <?php echo $applicant['jhs_awd']; ?></p>
+                            <p class=""><strong>Course Offered: </strong> <?php echo $applicant['shs_course']; ?></p>
                             </div>    
                         </div>
                         <div class="row">
                             <div class="col-12 col-sm-6">
-                            <p class="text-center"><strong>Start Year: </strong> <?php echo $applicant['jhs_start']; ?></p>
+                            <p class=""><strong>Start Year: </strong> <?php echo $applicant['shs_start']; ?></p>
                             </div>
                             <div class="col-12 col-sm-6">
-                            <p class="text-center"><strong>Year of Completion: </strong> <?php echo $applicant['jhs_end']; ?></p>
+                            <p class=""><strong>Year of Completion: </strong> <?php echo $applicant['shs_end']; ?></p>
                             </div>    
                         </div>
 
+                        <div class="row">
+                            <div class="col-12 col-sm-6">
+                            <p class=""><strong>Name of Tetiary Institution: </strong> <?php echo $applicant['tet_nm']; ?></p>
+                            </div>
+                            <div class="col-12 col-sm-6">
+                            <p class=""><strong>Course Offered: </strong> <?php echo $applicant['tet_course']; ?></p>
+                            </div>    
+                        </div>
+                        <div class="row">
+                            <div class="col-12 col-sm-6">
+                            <p class=""><strong>Start Year: </strong> <?php echo $applicant['tet_start']; ?></p>
+                            </div>
+                            <div class="col-12 col-sm-6">
+                            <p class=""><strong>Year of Completion: </strong> <?php echo $applicant['tet_end']; ?></p>
+                            </div>    
+                        </div>
 
+                        <div class="row">
+                            <div class="col-12 col-sm-6">
+                            <p class=""><strong>Previous Workplace: </strong> <?php echo $applicant['prev_wkplc']; ?></p>
+                            </div>
+                            <div class="col-12 col-sm-6">
+                            <p class=""><strong>Address: </strong> <?php echo $applicant['prev_wkplc_addr']; ?></p>
+                            </div>    
+                        </div>
+                        <div class="row">
+                            <div class="col-12 col-sm-6">
+                            <p class=""><strong>Position Held: </strong> <?php echo $applicant['position']; ?></p>
+                            </div>
+                            <div class="col-12 col-sm-6">
+                            <p class=""><strong>Start Date: </strong> <?php echo $applicant['prev_wkplc_start']; ?>
+                            <span class="mr-3"></span><strong>End Date: </strong> <?php echo $applicant['prev_wkplc_end']; ?></p>
+                            </div>    
+                        </div>
 
+                        <div class="row">
+                            <div class="col-12 col-sm-12">
+                            <p class=""><strong>Reasons For Leaving: </strong> <?php echo $applicant['reason']; ?></p>
+                            </div>
+                        </div>
 
+                        <div class="row">
+                            <div class="col-12 col-sm-6">
+                            <p class=""><strong>Name of Refree: </strong> <?php echo $applicant['ref_nm']; ?></p>
+                            </div>
+                            <div class="col-12 col-sm-6">
+                            <p class=""><strong>Contact: </strong> <?php echo $applicant['ref_cont']; ?></p>
+                            </div>    
+                        </div>
 
+                        <div class="row">
+                            <div class="col-12 col-sm-6">
+                            <p class=""><strong>Payment: </strong> <?php echo $applicant['paymentRadios']; ?></p>
+                            </div>
+                            <div class="col-12 col-sm-6">
+                            <p class=""><strong>Status: </strong> <?php echo $applicant['statusRadios']; ?></p>
+                            </div>    
+                        </div>
 
+                        <div class="row">
+                            <div class="col-12 col-sm-6">
+                            <p class=""><strong>Job Title: </strong> <?php echo $applicant['job_title']; ?></p>
+                            </div>
+                            <div class="col-12 col-sm-6">
+                            <p class=""><strong>Company: </strong> <?php echo $applicant['company']; ?></p>
+                            </div>    
+                        </div>
 
-                        <p class="text-center"><strong>Name: </strong> <?php echo $applicant['name']; ?>
-                        <span class="mr-5"></span><strong>Email: </strong><?php echo $applicant['email']; ?></p>
-                        <p class="text-center"><strong>Age: </strong><?php echo $applicant['age']; ?>
-                        <span class="mr-5 pr-3"></span><strong>Contact: </strong><?php echo $applicant['phone']; ?></p>
-                        <p class="text-center"><strong>Skill Level: </strong><?php echo $applicant['levelRadios']; ?>
-                        <span class="mr-5"></span><strong>Industry: </strong><?php echo $applicant['industry']; ?></p>
-                        <p class="text-center"><strong>Region: </strong><?php echo $applicant['region']; ?>
-                        <span class="mr-5"></span><strong>District: </strong><?php echo $applicant['district']; ?></p>
-                        <p class="text-center"><strong>Status: </strong><?php echo $applicant['statusRadios']; ?>
-                        <span class="mr-5"></span><strong>Company: </strong><?php echo $applicant['company']; ?></p>
                         <?php 
                         $user_name = $applicant['user_id'];
                         $sql = "SELECT * FROM users WHERE id='$user_name'";
@@ -175,14 +249,13 @@ if( !$_SESSION['email'] ){
                         ?>
                     </div>
                     <div class="card-footer">
-                    <a href="applicant_edit.php?id=<?php echo $applicant['id']; ?>" class="btn btn-sm btn-warning">Edit</a>
+                    <a href="applicant_edit.php?id=<?php echo $applicant['id']; ?>" class="btn btn-sm btn-primary">Update</a>
                     <a href="applicant_delete.php?id=<?php echo $applicant['id']; ?>" class="btn btn-sm btn-danger">Delete</a>
                     </div>
                         <?php    }
                         } else {
                             echo "0 results";
-                        }
-                        
+                        }                        
                         ?>
                     </div>
                 </div>
